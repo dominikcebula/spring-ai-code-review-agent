b78a5e6dd3e96918e7a61b29776c1651dabc672c
Dominik Cebula <dominikcebula@gmail.com>
2026-02-28 21:44:45 +0100

    spring-ai-code-review-agent

--- src/main/java/com/dominikcebula/spring/ai/agent/code/review/commands/PrReviewCommand.java
+++ src/main/java/com/dominikcebula/spring/ai/agent/code/review/commands/PrReviewCommand.java
@@ -1,6 +1,9 @@
 package com.dominikcebula.spring.ai.agent.code.review.commands;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 import org.springframework.stereotype.Component;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -10,11 +13,17 @@ import picocli.CommandLine.Option;
 @Slf4j
 public class PrReviewCommand implements Runnable {
 
-    @Option(names = "-pr-link", description = "PR Link", required = true)
+    @Option(names = "-pr-link", description = "PR Link", required = false)
     private String prLink;
 
+    @SneakyThrows
     @Override
     public void run() {
         log.info("Executing pr review command");
+
+        GitHub github = GitHubBuilder.fromEnvironment().build();
+
+
+        System.out.println(github.getMyself().getPublicRepoCount());
     }
 }
